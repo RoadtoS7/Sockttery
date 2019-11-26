e17f9b35a47b702b9f34eddb41b52d32da55519e
jihye0420 <gh06095@naver.com>
2019-11-27 05:14:29 +0900

    지혜 커밋

--- app/src/main/java/com/tistory/comfy91/sockttery/ui/LoginActivity.kt
+++ app/src/main/java/com/tistory/comfy91/sockttery/ui/LoginActivity.kt
@@ -7,16 +7,35 @@ import com.tistory.comfy91.sockttery.R
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    val REQUEST_CODE_LOGIN_ACTIVITY = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        init()
-    }
-    private fun init(){
-        btn_login.setOnClickListener{
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+        btn_login.setOnClickListener {
+            val edtId: String = edt_id.text.toString()
+            val edtPw: String = edt_password.text.toString()
+            if (edtId == "") edt_id.requestFocus()
+            else if(edtPw == "") edt_password.requestFocus()
+            else {
+                val intent: Intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
         }
+
+        btn_register.setOnClickListener {
+            val intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivityForResult(intent,REQUEST_CODE_LOGIN_ACTIVITY)
+
+        }
+//        init()
     }
+//    private fun init(){
+//        btn_login.setOnClickListener{
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 }
---
+++ app/src/main/java/com/tistory/comfy91/sockttery/ui/RegisterActivity.kt
@@ -0,0 +1,33 @@
+package com.tistory.comfy91.sockttery.ui
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tistory.comfy91.sockttery.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        btn_ok.setOnClickListener {
+            val edtId: String = edt_id.text.toString()
+            val edtPw: String = edt_password.text.toString()
+            if (edtId == "") edt_id.requestFocus()
+            else if (edtPw == "") edt_password.requestFocus()
+            else {
+                postRegisterResponse(edtId, edtPw)
+                finish()
+            }
+        }
+    }
+    fun postRegisterResponse(user_id: String, user_password: String) {
+        val intent: Intent = Intent()
+        setResult(Activity.RESULT_OK, intent)
+        //값 넘기기
+        finish()
+    }
+}
